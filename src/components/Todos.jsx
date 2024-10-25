4b7d3c3c3bddd32188cfe3f63baad0577a890c84
Mohirjon013 <mohirjon26@gmail.com>
2024-10-25 15:01:35 +0100

    commit

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,5 +1,5 @@
 import { Button, TextField } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addTodo, updateTodo } from '../redtool/slices/crudSlice'
 import CustomTable from './CustomTable'
@@ -13,6 +13,7 @@ function Todos() {
     const [editModal, setEditModal] = useState(false)
     const [updateValue, setUpdateValue] = useState("")
     const [editId, setEditId] = useState(null)
+    const [filterTodo, setFilterTodo] = useState(todos)
 
     const tableHeadValue = [
         {
@@ -30,6 +31,7 @@ function Todos() {
         }
     ]
 
+    // add todo
     function handleSubmit(e){
         e.preventDefault()
         const data = {
@@ -39,7 +41,7 @@ function Todos() {
         dispatch(addTodo(data))
         setTodoValue("")
     }
-
+    // update todo
     function handleUpdateBtn(e){
         e.preventDefault()
         console.log("salom");
@@ -50,12 +52,23 @@ function Todos() {
         
         
     }
+    // search todo
+    function handleSearch(e){
+        const filteredInput = todos.filter(item => item.title.toLowerCase().includes(e.target.value.toLowerCase()))
+        setFilterTodo(filteredInput)        
+    }
+    useEffect(() => {
+        setFilterTodo(todos)
+    },[todos])
     return (
         <>
             <form onSubmit={handleSubmit} autoComplete='off' className='w-[400px] mx-auto mt-7 flex gap-3 items-center'>
-                <TextField size='small' className='w-full' value={todoValue} onChange={(e) => setTodoValue(e.target.value)} variant='outlined' type='text' label='Add todo' ></TextField>
+                <TextField size='small' className='w-full' value={todoValue} onChange={(e) => setTodoValue(e.target.value)} variant='outlined' type='text' label='Add todo' />
                 <Button size='medium' className='py-7.5' type='submit'variant='contained' >Submit</Button>
             </form>
+            <div className="w-[350px] mx-auto mt-5">
+                <TextField autoComplete='off' onInput={handleSearch} size='small' className='w-full ' variant='outlined' type='text' label='searching'  />
+            </div>
             <div className="w-[500px] mx-auto mt-10 ">
                 {/* {todos && todos.map(item => (
                     <div key={item.id} className='flex gap-3'>
@@ -63,10 +76,10 @@ function Todos() {
                         <p>{item.title}</p>
                     </div>
                 ))} */}
-                <CustomTable tHead={tableHeadValue} rows={todos} setUpdateValue={setUpdateValue} setEditId={setEditId} setEditModal={setEditModal}/>
+                <CustomTable tHead={tableHeadValue} rows={filterTodo} setUpdateValue={setUpdateValue} setEditId={setEditId} setEditModal={setEditModal}/>
             </div>
             <CustomModal open={editModal} setOpen={setEditModal}>
-                <form onSubmit={handleUpdateBtn}  className='flex items-center gap-2'>
+                <form onSubmit={handleUpdateBtn} autoComplete='off'  className='flex items-center py-2 gap-2'>
                     <TextField value={updateValue || ""} onChange={(e) => setUpdateValue(e.target.value)} variant='outlined' size='small' sx={{width:"100%"}} id="outlined-basic" type='text' label="Update todos" />
                     <Button size='medium' className='py-7.5' type='submit'variant='contained' >Update</Button>
                 </form> 
